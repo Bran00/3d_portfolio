40887c09a06e5726fbfd660e6c8e6a89ef9cc4ff
Brando Rocha <86134682+Bran00@users.noreply.github.com>
2023-04-14 20:53:12 -0300

    fix: remove pokenext

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,35 +1,35 @@
-import logo from "./logo.svg";
-import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
-import web from "./web.png";
-import github from "./github.png";
-import menu from "./menu.svg";
-import close from "./close.svg";
+import logo from "./logo.svg"
+import backend from "./backend.png"
+import creator from "./creator.png"
+import mobile from "./mobile.png"
+import web from "./web.png"
+import github from "./github.png"
+import menu from "./menu.svg"
+import close from "./close.svg"
 
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
+import css from "./tech/css.png"
+import docker from "./tech/docker.png"
+import figma from "./tech/figma.png"
+import git from "./tech/git.png"
+import html from "./tech/html.png"
+import javascript from "./tech/javascript.png"
+import mongodb from "./tech/mongodb.png"
+import nodejs from "./tech/nodejs.png"
+import reactjs from "./tech/reactjs.png"
+import redux from "./tech/redux.png"
+import tailwind from "./tech/tailwind.png"
+import typescript from "./tech/typescript.png"
+import threejs from "./tech/threejs.svg"
 
-import meta from "./company/meta.png";
-import costs from "./costs.png";
-import repoWars from "./repoWars.png";
-import metaverse from "./metaverse.png";
-import tesla from "./company/tesla.png";
+import meta from "./company/meta.png"
+import costs from "./costs.png"
+import repoWars from "./repoWars.png"
+import metaverse from "./metaverse.png"
+import tesla from "./company/tesla.png"
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
+import carrent from "./carrent.png"
+import jobit from "./jobit.png"
+import tripguide from "./tripguide.png"
 
 export {
   logo,
@@ -55,11 +55,10 @@ export {
   threejs,
   costs,
   repoWars,
-  pokenext,
   metaverse,
   meta,
   tesla,
   carrent,
   jobit,
   tripguide,
-};
+}
